145c52717b43410e21bfc44ba219799d2c7876ef
mbessett <mbessett@ualberta.ca>
2012-03-14 21:08:30 -0600

    Fleshy DatabaseEntry extensions

--- src/mole/finder/ConditionEntry.java
+++ src/mole/finder/ConditionEntry.java
@@ -1,13 +1,51 @@
 package mole.finder;
 
+import java.io.ByteArrayOutputStream;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
+
 
 public class ConditionEntry extends DatabaseEntry {
+	// database rows
+	private String tag, comment;
+	private Bitmap image;
 
-	public ConditionEntry(int id) {
+	// constructor 1 (comment is optional)
+	public ConditionEntry(int id, String tag, byte[] image) {
 		super(id);
-		// TODO Auto-generated constructor stub
+		this.setTag(tag);
+		this.image = BitmapFactory.decodeByteArray(image, 0, image.length); 
 	}
+	// constructor 2
+	public ConditionEntry(int id, String tag, byte[] image, String comment) {
+		this(id, tag, image);
+		this.setComment(comment);
+	}
+
+	// return the image as a byte array, use for saving entry to DB
+	public byte[] getBitmapAsByteArray() { 
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream(); 
+		this.image.compress(CompressFormat.PNG, 0, outputStream); 
+		return outputStream.toByteArray(); 
+	} 
 	
-	
-	
+	// getters/setters, changing the image doesn't make any
+	// sense => only need to set on tag and comment
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public Bitmap getImage() {
+		return this.image;
+	}
 }
--- src/mole/finder/MoleFinderModel.java
+++ src/mole/finder/MoleFinderModel.java
@@ -1,5 +1,17 @@
 package mole.finder;
 
-public class MoleFinderModel {
+import java.util.ArrayList;
 
+/** 
+ * 
+ * @author mbessett
+ *
+ */
+
+public class MoleFinderModel {
+	// db access
+	private DatabaseManager DBManager;
+	// active entries - to store after a search
+	private ArrayList<ConditionEntry> conditions;
+	private ArrayList<ConditionTag> tags;
 }
